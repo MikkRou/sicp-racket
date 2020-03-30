3c936fc459b906299dcf68071fc85916b34bbfef
MikkRou <mikhail.rubashkin@gmail.com>
2020-03-31 00:39:06 +0200

    Fix exercise 17, chapter1 (recursive process, as with fast-exp).

--- sicp/chapter1/17.rkt
+++ sicp/chapter1/17.rkt
@@ -13,7 +13,7 @@
 
 (define (fast-* a b)
   (cond ((= b 0) 0)
-        ((even? b) (fast-* (double a) (half b)))
+        ((even? b) (double (fast-* a (half b))))
         (else (+ a (fast-* a (- b 1))))))
 
 (define (even? num)
